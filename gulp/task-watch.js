0ff4af962b1ef7a8d1d218b65212be230c2a1217
BlueSkyFish <blueskyfish@blueskyfish.de>
2015-12-01 18:00:10 +0100

    gulp: add the watch-index task

--- gulp/task-watch.js
+++ gulp/task-watch.js
@@ -16,7 +16,7 @@ var settings = require('./gulp-settings');
 gulp.task('watch', function () {
   gulp.watch(config.index.sources, ['watch-index']);
   gulp.watch(config.styles.sources, ['watch-styles']);
-  gulp.watch(config.scripts.sources, ['verify-scripts', 'watch-scripts']);
+  gulp.watch(config.scripts.sources, ['watch-index', 'verify-scripts', 'watch-scripts']);
   gulp.watch(config.templates.sources, ['watch-templates']);
 });
 
